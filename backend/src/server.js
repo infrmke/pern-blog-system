49a5e23664e54c21688c31c46eb3c6fb68f00b95
Inaros Lemuel <infromke@gmail.com>
2025-12-30 18:17:14 -0300

    import db connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import express from 'express'
+import { connectToDb } from './config/database.js'
 
 const app = express()
 const PORT = process.env.PORT || 3001
+
+connectToDb()
 
 app.listen(PORT, () =>
   console.log(`[SERVER] up and running at http://localhost:${PORT}`)
