f5358d3be109463121c3001b7ff2fce2e23f11b7
Inaros Lemuel <sentidosheep@gmail.com>
2025-12-31 03:12:01 -0300

    add user router

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,17 @@
 import express from 'express'
+
 import { connectToDb } from './config/database.js'
+
+import UserRouter from './modules/user/user.route.js'
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.use(express.json())
+
 connectToDb()
+
+app.use('/users', UserRouter)
 
 app.listen(PORT, () =>
   console.log(`[SERVER] up and running at http://localhost:${PORT}`)
