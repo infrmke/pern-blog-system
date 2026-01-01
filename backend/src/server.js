4c314e3d9fd6c0dc9faf3951c02037fa3f5b9f7a
Inaros Lemuel <sentidosheep@gmail.com>
2025-12-31 23:43:51 -0300

    add auth router

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,21 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
 
 import { connectToDb } from './config/database.js'
 
 import UserRouter from './modules/user/user.route.js'
+import AuthRouter from './modules/auth/auth.route.js'
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(express.json())
+app.use(cookieParser())
 
 connectToDb()
 
 app.use('/users', UserRouter)
+app.use('/auth', AuthRouter)
 
 app.listen(PORT, () =>
   console.log(`[SERVER] up and running at http://localhost:${PORT}`)
