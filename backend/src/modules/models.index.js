a0c30fa08ed72be0be00ca3794e3d3f992b27ccd
Inaros Lemuel <sentidosheep@gmail.com>
2026-01-06 03:28:14 -0300

    add comment associations

--- backend/src/modules/models.index.js
+++ backend/src/modules/models.index.js
@@ -1,10 +1,17 @@
 import User from './user/user.model.js'
 import Post from './post/post.model.js'
+import Comment from './comment/comment.model.js'
 
 // RELAÇÕES DE USER
 User.hasMany(Post, { as: 'posts', foreignKey: 'authorId' })
+User.hasMany(Comment, { as: 'comments', foreignKey: 'userId' })
 
 // RELAÇÕES DE POST
 Post.belongsTo(User, { as: 'author', foreignKey: 'authorId' })
+Post.hasMany(Comment, { as: 'comments', foreignKey: 'postId' })
 
-export { User, Post }
+// RELAÇÕES DE COMMENT
+Comment.belongsTo(User, { as: 'user', foreignKey: 'userId' })
+Comment.belongsTo(Post, { as: 'post', foreignKey: 'postId' })
+
+export { User, Post, Comment }
