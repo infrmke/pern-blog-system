e24231be64890aed5790065cfb0e11ae803fecc9
Inaros Lemuel <sentidosheep@gmail.com>
2026-01-05 23:48:10 -0300

    add post router

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ import { connectToDb } from './config/database.js'
 
 import UserRouter from './modules/user/user.route.js'
 import AuthRouter from './modules/auth/auth.route.js'
+import PostRouter from './modules/post/post.route.js'
 
 const app = express()
 const PORT = process.env.PORT || 3001
@@ -16,6 +17,7 @@ connectToDb()
 
 app.use('/users', UserRouter)
 app.use('/auth', AuthRouter)
+app.use('/posts', PostRouter)
 
 app.listen(PORT, () =>
   console.log(`[SERVER] up and running at http://localhost:${PORT}`)
