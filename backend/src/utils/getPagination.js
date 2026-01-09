9426a8d8a5622a1c5bcda1426ef060b54a9b5136
Inaros Lemuel <infromke@gmail.com>
2026-01-08 23:04:12 -0300

    create getPagination util

---
+++ backend/src/utils/getPagination.js
@@ -0,0 +1,9 @@
+const getPagination = (query) => {
+  const page = parseInt(query.page) || 1
+  const limit = parseInt(query.limit) || 10
+  const offset = (page - 1) * limit
+
+  return { page, limit, offset }
+}
+
+export default getPagination
