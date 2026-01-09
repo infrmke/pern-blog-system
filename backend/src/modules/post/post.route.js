21f57ce5c889d834a52c2c2afe394fd591c533a5
Inaros Lemuel <infromke@gmail.com>
2026-01-09 19:04:47 -0300

    add validation middleware to routes

--- backend/src/modules/post/post.route.js
+++ backend/src/modules/post/post.route.js
@@ -5,6 +5,7 @@ import PostController from './post.controller.js'
 import verifyAccessToken from '../../middlewares/verifyAccessToken.js'
 import isAdmin from '../../middlewares/isAdmin.js'
 import { verifyPostOwnership } from '../../middlewares/verifyOwnership.js'
+import { createPostValidator, updatePostValidator } from './post.validator.js'
 
 const router = Router()
 
@@ -13,12 +14,19 @@ router.get('/search', PostController.getByTitle)
 router.get('/author', PostController.getByAuthor)
 router.get('/slug/:slug', PostController.getBySlug)
 router.get('/:id', PostController.getById)
-router.post('/', verifyAccessToken, isAdmin, PostController.create)
+router.post(
+  '/',
+  verifyAccessToken,
+  isAdmin,
+  createPostValidator,
+  PostController.create
+)
 router.patch(
   '/:id',
   verifyAccessToken,
   isAdmin,
   verifyPostOwnership,
+  updatePostValidator,
   PostController.update
 )
 router.delete(
