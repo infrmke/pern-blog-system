c924481bf30cf6a48e4528596cc645859f2c6883
Inaros Lemuel <infromke@gmail.com>
2026-01-09 17:13:29 -0300

    add validation middleware to routes

--- backend/src/modules/session/session.route.js
+++ backend/src/modules/session/session.route.js
@@ -2,11 +2,12 @@ import { Router } from 'express'
 
 import SessionController from './session.controller.js'
 import verifyAccessToken from '../../middlewares/verifyAccessToken.js'
+import loginValidator from './session.validator.js'
 
 const router = Router()
 
 router.get('/me', verifyAccessToken, SessionController.status)
-router.post('/login', SessionController.logIn)
+router.post('/login', loginValidator, SessionController.logIn)
 router.post('/logout', verifyAccessToken, SessionController.logOut)
 
 export default router
--- backend/src/modules/user/user.route.js
+++ backend/src/modules/user/user.route.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import UserController from './user.controller.js'
+import { registerValidator, updateValidator } from './user.validator.js'
 
 const router = Router()
 
-router.post('/', UserController.create)
+router.post('/', registerValidator, UserController.create)
 router.get('/', UserController.getAll)
 router.get('/:id', UserController.getById)
-router.patch('/:id', UserController.update)
+router.patch('/:id', updateValidator, UserController.update)
 router.delete('/:id', UserController.delete)
 router.get('/profile/:slug', UserController.getBySlug)
 
