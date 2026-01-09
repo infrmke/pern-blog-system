041d40644f0e7a5ff78071c94c0ee731f53573d0
Inaros Lemuel <infromke@gmail.com>
2026-01-09 17:13:03 -0300

    create user validation middleware with express-validator

---
+++ backend/src/modules/user/user.validator.js
@@ -0,0 +1,97 @@
+import { validationResult, body } from 'express-validator'
+import throwHttpError from '../../utils/throwHttpError.js'
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req)
+
+  if (!errors.isEmpty()) {
+    const firstError = errors.array()[0].msg // pega apenas a primeira mensagem de erro
+    throwHttpError(400, firstError, 'VALIDATION_ERROR')
+  }
+
+  next()
+}
+
+const registerValidator = [
+  body('name')
+    .trim()
+    .notEmpty()
+    .withMessage('Name cannot be empty.')
+    .isLength({ min: 2, max: 56 })
+    .withMessage('Name must be between 2 and 56 characters.'),
+
+  body('email')
+    .trim()
+    .normalizeEmail() // retira pontos extras e converte a string para minúscula
+    .notEmpty()
+    .withMessage('Email cannot be empty.')
+    .isEmail()
+    .withMessage('Provide a valid e-mail address.'),
+
+  body('password')
+    .notEmpty()
+    .withMessage('Password cannot be empty.')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters.'),
+
+  body('confirm_password')
+    .notEmpty()
+    .withMessage('Confirm your password.')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords must match each other.')
+      }
+      return true
+    }),
+
+  validate,
+]
+
+const updateValidator = [
+  body('name')
+    .optional()
+    .trim()
+    .notEmpty()
+    .withMessage('Name cannot be empty.')
+    .isLength({ min: 2, max: 54 })
+    .withMessage('Name must be between 2 and 56 characters.'),
+
+  body('email')
+    .optional()
+    .trim()
+    .normalizeEmail() // retira pontos extras e converte a string para minúscula
+    .notEmpty()
+    .withMessage('Email cannot be empty.')
+    .isEmail()
+    .withMessage('Provide a valid e-mail address.'),
+
+  body('avatar')
+    .optional()
+    .trim()
+    .isString()
+    .withMessage('Avatar must be a string.')
+    .notEmpty()
+    .withMessage('Avatar cannot be empty.'),
+
+  body('password')
+    .optional()
+    .notEmpty()
+    .withMessage('Password cannot be empty.')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters.'),
+
+  body('confirm_password')
+    .if(body('password').exists()) // apenas valida se "password" for enviado
+    .notEmpty()
+    .withMessage('Confirm your password.')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Passwords must match each other.')
+      }
+      return true
+    }),
+
+  validate,
+]
+
+export { registerValidator, updateValidator }
