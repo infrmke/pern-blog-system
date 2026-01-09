bc44a4c36ede65ce9d9763b1318ea045a28dd335
Inaros Lemuel <infromke@gmail.com>
2026-01-09 18:26:28 -0300

    add validation middleware to route

--- backend/src/modules/comment/comment.route.js
+++ backend/src/modules/comment/comment.route.js
@@ -3,15 +3,22 @@ import { Router } from 'express'
 import CommentController from './comment.controller.js'
 import verifyAccessToken from '../../middlewares/verifyAccessToken.js'
 import { verifyCommentOwnership } from '../../middlewares/verifyOwnership.js'
+import commentValidator from './comment.validator.js'
 
 const router = Router()
 
 router.get('/post/:postId', CommentController.getByPost)
-router.post('/post/:postId', verifyAccessToken, CommentController.create)
+router.post(
+  '/post/:postId',
+  verifyAccessToken,
+  commentValidator,
+  CommentController.create
+)
 router.patch(
   '/:id',
   verifyAccessToken,
   verifyCommentOwnership,
+  commentValidator,
   CommentController.update
 )
 router.delete(
