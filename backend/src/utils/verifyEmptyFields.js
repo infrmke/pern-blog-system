a50c534fdf0f4a26f4bc21117c806e8f86fc33be
Inaros Lemuel <infromke@gmail.com>
2026-01-10 00:33:38 -0300

    delete verifyEmptyFields utility

--- backend/src/utils/verifyEmptyFields.js
+++
@@ -1,9 +0,0 @@
-const verifyEmptyFields = (data) => {
-  const emptyField = Object.entries(data).find(
-    ([key, value]) => typeof value === 'string' && value.trim() === ''
-  )
-  // retorna apenas o primeiro campo vazio detectado ou null
-  return emptyField ? emptyField[0] : null
-}
-
-export default verifyEmptyFields
