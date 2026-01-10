dfe4e0e56f98734fa20e9a8b3cfc22cad8dce2f2
Inaros Lemuel <infromke@gmail.com>
2026-01-10 00:51:54 -0300

    create verifyUserOwnership middleware

--- backend/src/middlewares/verifyOwnership.js
+++ backend/src/middlewares/verifyOwnership.js
@@ -2,6 +2,18 @@ import { Post, Comment } from '../modules/models.index.js'
 import canModify from '../utils/canModify.js'
 
 /**
+ * Verifica se o usuário autenticado é o proprietário da conta passada pelo ID na URL.
+ */
+const verifyUserOwnership = (req, res, next) => {
+  if (req.params.id !== req.user.id) {
+    return res
+      .status(403)
+      .json({ message: 'You can only modify your own account.' })
+  }
+  next()
+}
+
+/**
  * Verifica se o usuário autenticado é o proprietário do Post identificado pelo ID na URL.
  * Adiciona a instância do post ao objeto `req` para evitar consultas duplicadas.
  */
@@ -47,4 +59,4 @@ const verifyCommentOwnership = async (req, res, next) => {
   }
 }
 
-export { verifyPostOwnership, verifyCommentOwnership }
+export { verifyUserOwnership, verifyPostOwnership, verifyCommentOwnership }
