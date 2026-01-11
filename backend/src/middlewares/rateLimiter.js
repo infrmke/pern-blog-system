37139107bb17492c1a76a3d69f48a7d0bebfca41
Inaros Lemuel <infromke@gmail.com>
2026-01-11 05:10:43 -0300

    create rate limiters for authentication and content creation

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -1,7 +1,7 @@
 import rateLimit from 'express-rate-limit'
 import throwHttpError from '../utils/throwHttpError.js'
 
-const rateLimiter = rateLimit({
+const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
   max: 200, // apenas 200 requisições por janela
   handler: (req, res, next) => {
@@ -19,4 +19,40 @@ const rateLimiter = rateLimit({
   legacyHeaders: false,
 })
 
-export default rateLimiter
+const sessionLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutos
+  max: 10, // apenas 10 requisições por janela, para evitar brute force
+  handler: (req, res, next) => {
+    try {
+      throwHttpError(
+        429,
+        'You are sending too many requests. Please try again later.',
+        'TOO_MANY_REQUESTS'
+      )
+    } catch (error) {
+      next(error)
+    }
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+})
+
+const contentLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hora
+  max: 20, // apenas 20, para evitar spam
+  handler: (req, res, next) => {
+    try {
+      throwHttpError(
+        429,
+        'You are sending too many requests. Please try again later.',
+        'TOO_MANY_REQUESTS'
+      )
+    } catch (error) {
+      next(error)
+    }
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+})
+
+export { globalLimiter, sessionLimiter, contentLimiter }
