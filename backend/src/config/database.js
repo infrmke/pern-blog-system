f3083614a48b2d9936481820eb0c8ab021166739
Inaros Lemuel <infromke@gmail.com>
2026-01-11 17:29:22 -0300

    sequelize-cli: project bootstrapping

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,17 @@
 import { Sequelize } from 'sequelize'
 
+// para o sequelize-cli
+const config = {
+  username: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: 5432,
+  logging: false,
+}
+
+// para o aplicativo
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -15,11 +27,16 @@ const sequelize = new Sequelize(
 const connectToDb = async () => {
   try {
     await sequelize.authenticate()
-    await sequelize.sync({ alter: true })
     console.log('[SEQUELIZE] Database connected and synchronized.')
+    sequelize.sync({ alter: true })
   } catch (error) {
     console.error('\n[SEQUELIZE] Connection error:', error)
   }
 }
 
-export { sequelize, connectToDb }
+// exportação do objeto config puro para o .sequelizerc
+module.exports = config
+
+// exportações nomeadas para o resto da aplicação
+module.exports.sequelize = sequelize
+module.exports.connectToDb = connectToDb
