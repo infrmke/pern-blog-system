f64dd4e993ac2d265cf6d9f2d39f23a76cb44204
Inaros Lemuel <infromke@gmail.com>
2026-01-17 23:51:58 -0300

    create optimizeImage middleware

---
+++ backend/src/middlewares/optimizeImage.js
@@ -0,0 +1,20 @@
+import processImage from '../utils/processImage.js'
+
+const optimizeImage = async (req, res, next) => {
+  if (!req.file) return next()
+
+  try {
+    // define larguras diferentes para avatar e banner
+    const width = req.uploadFolder === 'avatars' ? 500 : 1200
+
+    // processa a imagem e a atualiza com .webp
+    const optimizedName = await processImage(req.file, req.uploadFolder, width)
+    req.file.filename = optimizedName
+
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+export default optimizeImage
