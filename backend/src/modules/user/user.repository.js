51ebe1c528ea4dfcdc5c9a22b61aa2db7ec720d8
Inaros Lemuel <infromke@gmail.com>
2026-01-19 02:26:41 -0300

    create repository layer for user module

---
+++ backend/src/modules/user/user.repository.js
@@ -0,0 +1,35 @@
+import User from './user.model.js'
+
+class UserRepository {
+  async create(data) {
+    const user = await User.create(data)
+    return user
+  }
+
+  async findOne(filter) {
+    const user = await User.findOne({ where: filter })
+    return user
+  }
+
+  async findAndCountAll(limit, offset, order) {
+    const { count, rows: users } = await User.findAndCountAll(limit, offset, order)
+    return { count, users }
+  }
+
+  async findByPk(id) {
+    const user = await User.findByPk(id)
+    return user
+  }
+
+  async update(id, data) {
+    const user = await User.findByPk(id)
+    return user ? await user.update(data) : null
+  }
+
+  async remove(filter) {
+    const user = await User.destroy({ where: filter })
+    return user
+  }
+}
+
+export default new UserRepository()
