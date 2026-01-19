9b4d36e38295ada4ef2a5df1f5d0d706034f0a5c
Inaros Lemuel <infromke@gmail.com>
2026-01-19 03:54:29 -0300

    feature: integrate verifyUploadFolders into app config

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,12 @@ import cookieParser from 'cookie-parser'
 import { connectToDb } from './config/database.js'
 import globalRouter from './modules/index.routes.js'
 import errorHandler from './middlewares/errorHandler.js'
+import verifyUploadFolders from './utils/verifyUploadFolders.js'
 
 // config
 const app = express()
+
+verifyUploadFolders() // verifica se a pasta "uploads" com "avatars" e "banners" existe
 
 app.use(express.json())
 app.use(cookieParser())
