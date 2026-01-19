d792ab1d28c8d57366bcb7c474db988b693d1b41
Inaros Lemuel <infromke@gmail.com>
2026-01-19 01:05:35 -0300

    replace multer's diskStorage with memoryStorage

--- backend/src/config/multer.js
+++ backend/src/config/multer.js
@@ -1,19 +1,6 @@
 import multer from 'multer'
-import path from 'node:path'
-import crypto from 'node:crypto'
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const folder = req.uploadFolder || 'misc' // o handleUpload vai definir se o destino é "avatars" ou "banners"
-    cb(null, path.resolve('uploads', folder))
-  },
-  filename: (req, file, cb) => {
-    const uniquePrefix = crypto.randomBytes(10).toString('hex') // nome único para evitar conflito
-    const extension = path.extname(file.originalname)
-
-    cb(null, `${uniquePrefix}${extension}`)
-  },
-})
+const storage = multer.memoryStorage()
 
 const fileFilter = (req, file, callback) => {
   const allowedMimes = ['image/jpeg', 'image/png', 'image/webp']
