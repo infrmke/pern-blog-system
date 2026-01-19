8a593e609b108b0fe31cb2131a3cf9096a731494
Inaros Lemuel <infromke@gmail.com>
2026-01-19 01:43:28 -0300

    fix output path for optimized image using __dirname

--- backend/src/utils/processImage.js
+++ backend/src/utils/processImage.js
@@ -1,6 +1,11 @@
 import sharp from 'sharp'
 import crypto from 'node:crypto'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+// configurando __dirname pra funcionar em module
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const processImage = async (file, folder, width) => {
   // gera um nome único (ex.: d39e01db0d1dda1fcc33), já com a extensão final
@@ -8,7 +13,9 @@ const processImage = async (file, folder, width) => {
   const newFileName = `${uniqueName}.webp`
 
   // define o caminho onde a imagem otimizada será salva
-  const outputPath = path.resolve('uploads', folder, newFileName)
+  const outputPath = path.resolve(__dirname, '..', 'uploads', folder, newFileName)
+
+  console.log(outputPath)
 
   await sharp(file.buffer) // lê o arquivo de imagem direto da RAM
     .resize(width) // redimensiona a imagem
