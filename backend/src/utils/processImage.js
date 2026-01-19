69be328c7c3bc385fff60bcdf8e03dfef972cb1d
Inaros Lemuel <infromke@gmail.com>
2026-01-19 01:50:22 -0300

    remove debug log for output path

--- backend/src/utils/processImage.js
+++ backend/src/utils/processImage.js
@@ -15,8 +15,6 @@ const processImage = async (file, folder, width) => {
   // define o caminho onde a imagem otimizada será salva
   const outputPath = path.resolve(__dirname, '..', 'uploads', folder, newFileName)
 
-  console.log(outputPath)
-
   await sharp(file.buffer) // lê o arquivo de imagem direto da RAM
     .resize(width) // redimensiona a imagem
     .webp({ quality: 80 }) // converte para .webp com 80% de qualidade
