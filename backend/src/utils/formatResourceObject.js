cada7225b4a63bf4896aa0ce0f8bc812a8a1c544
Inaros Lemuel <infromke@gmail.com>
2026-01-20 03:32:33 -0300

    refactor: optimize formatCommentObject to conditionally include user and post

--- backend/src/utils/formatResourceObject.js
+++ backend/src/utils/formatResourceObject.js
@@ -51,20 +51,24 @@ const formatCommentObject = (comment) => ({
   content: comment.content,
   createdAt: comment.createdAt,
   updatedAt: comment.updatedAt,
-  user: comment.user
-    ? {
-        id: comment.user.id,
-        name: comment.user.name,
-        slug: comment.user.slug,
-      }
-    : null,
-  post: comment.post
-    ? {
-        id: comment.post.id,
-        title: comment.post.title,
-        slug: comment.post.slug,
-      }
-    : null,
+
+  // só adiciona a propriedade user se ela existir
+  ...(comment.user && {
+    user: {
+      id: comment.user.id,
+      name: comment.user.name,
+      slug: comment.user.slug,
+    },
+  }),
+
+  // só adiciona a propriedade post se ela existir
+  ...(comment.post && {
+    post: {
+      id: comment.post.id,
+      title: comment.post.title,
+      slug: comment.post.slug,
+    },
+  }),
 })
 
 export { formatUserObject, formatPostObject, formatCommentObject }
