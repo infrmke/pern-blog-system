637f9a1aaa264185b27bdbc6d29c0b97ad336a87
Inaros Lemuel <infromke@gmail.com>
2026-01-20 04:54:23 -0300

    refactor: apply Controller-Service-Repository pattern

--- backend/src/modules/postLike/postLike.controller.js
+++ backend/src/modules/postLike/postLike.controller.js
@@ -1,5 +1,4 @@
-import { Post, PostLike } from '../index.models.js'
-import throwHttpError from '../../utils/throwHttpError.js'
+import PostLikeService from './postLike.service.js'
 
 class PostLikeController {
   async toggle(req, res, next) {
@@ -7,22 +6,12 @@ class PostLikeController {
     const { postId } = req.params
 
     try {
-      const post = await Post.findByPk(postId)
+      const like = await PostLikeService.toggle(postId, userId)
 
-      if (!post) throwHttpError(404, 'Post not found.', 'POST_NOT_FOUND')
-
-      const existingLike = await PostLike.findOne({
-        where: { userId, postId },
-      })
-
-      // caso o usuário já tenha dado "like", remove esse mesmo like
-      if (existingLike) {
-        await existingLike.destroy()
+      if (!like) {
         return res.status(200).json({ message: 'Post disliked.' })
       }
 
-      // caso o usuário não tenha dado like, "cria" um like
-      await PostLike.create({ userId, postId })
       return res.status(201).json({ message: 'Post liked.' })
     } catch (error) {
       next(error)
