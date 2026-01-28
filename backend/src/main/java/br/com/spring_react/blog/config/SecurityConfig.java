5869098cc026a0f887e922409866562fd0cf1f1c
Inaros Lemuel <infromke@gmail.com>
2026-01-28 15:28:01 -0300

    add security config to temporarily allow unauthenticated API requests

---
+++ backend/src/main/java/br/com/spring_react/blog/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package br.com.spring_react.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // desabilita o CSRF
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // não pede autenticação para rotas
+                );
+
+        return http.build();
+    }
+}
