08ffd6837f1a06a4b7001c8bda0ff49d2f22369c
Inaros Lemuel <infromke@gmail.com>
2026-01-28 22:02:29 -0300

    define validation constraints and add "confirmPassword" arg to UserUpdateDTO

--- backend/src/main/java/br/com/spring_react/blog/user/UserUpdateDTO.java
+++ backend/src/main/java/br/com/spring_react/blog/user/UserUpdateDTO.java
@@ -1,6 +1,18 @@
 package br.com.spring_react.blog.user;
 
-public record UserUpdateDTO(String name,
-                            String email,
-                            String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public record UserUpdateDTO(
+        @Size(min = 2, max = 80, message = "Name must be between 2 and 80 characters")
+        String name,
+
+        @Email(message = "Invalid email format")
+        String email,
+
+        @Size(min = 8, message = "Password must be at least 8 characters")
+        String password,
+
+        String confirmPassword
+) {
 }
