401a33b8546ae2b146c170d6622a9cdc921b7318
Inaros Lemuel <infromke@gmail.com>
2026-01-29 21:17:24 -0300

    create service layer for Session module

---
+++ backend/src/main/java/br/com/spring_react/blog/session/SessionService.java
@@ -0,0 +1,31 @@
+package br.com.spring_react.blog.session;
+
+import br.com.spring_react.blog.user.UserService;
+import br.com.spring_react.blog.user.internal.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionService {
+
+    private final UserService userService;
+    private final TokenService tokenService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public SessionService(UserService userService, TokenService tokenService,
+                          BCryptPasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.tokenService = tokenService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public String authenticate(LoginRequestDTO data) {
+        User user = userService.findByEmailForAuth(data.email());
+
+        if (!passwordEncoder.matches(data.password(), user.getPassword())) {
+            throw new RuntimeException("Invalid credentials.");
+        }
+
+        return tokenService.generateToken(user);
+    }
+}
